81fc2c06b905a04aedb8bfa5e708de6a764614dc
Matthew Scharley <matt.scharley@gmail.com>
2015-11-03 08:49:12 +1100

    Add PHPCS test

--- pakefile.php
+++ pakefile.php
@@ -1,13 +1,17 @@
 <?php
 
 pake_desc('Run CI tasks');
-pake_task('ci', 'phpunit');
-
+pake_task('ci', 'phpcs', 'phpunit');
 function run_ci() {}
+
+pake_desc('Run PHPCS');
+pake_task('phpcs');
+function run_phpcs() {
+  pake_sh('vendor/bin/phpcs lib');
+}
 
 pake_desc('Run PHPUnit tests');
 pake_task('phpunit');
-
 function run_phpunit() {
-  system('vendor/bin/phpunit tests');
+  pake_sh('vendor/bin/phpunit tests');
 }
