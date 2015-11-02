3ded9bd3e10bfc90fc17642d2f58561bd9f62381
Matthew Scharley <matt.scharley@gmail.com>
2015-11-03 09:23:53 +1100

    Add PHPMD task

--- pakefile.php
+++ pakefile.php
@@ -7,7 +7,13 @@ function run_ci() {}
 pake_desc('Run PHPCS');
 pake_task('phpcs');
 function run_phpcs() {
-  pake_sh('vendor/bin/phpcs lib');
+  pake_sh('vendor/bin/phpcs lib tests');
+}
+
+pake_desc('Run PHPMD');
+pake_task('phpmd');
+function run_phpmd() {
+  pake_sh('vendor/bin/phpmd lib,tests text cleancode,codesize,controversial,design,unusedcode');
 }
 
 pake_desc('Run PHPUnit tests');
