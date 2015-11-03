e4290b3f974fb89a6a967990ecb3692caadc4e21
Matthew Scharley <matt.scharley@gmail.com>
2015-11-03 13:02:12 +1100

    Implement code climate code coverage

--- pakefile.php
+++ pakefile.php
@@ -25,5 +25,9 @@ function run_phpmd() {
 pake_desc('Run PHPUnit tests');
 pake_task('phpunit');
 function run_phpunit() {
-  pake_sh('vendor/bin/phpunit tests');
+  $cc = !empty(getenv('CODECLIMATE_REPO_TOKEN'));
+  pake_sh('vendor/bin/phpunit tests' . ($cc ? ' --coverage-clover build/logs/clover.xml' : ''));
+  if ($cc) {
+    pake_sh('vendor/bin/test-reporter');
+  }
 }
